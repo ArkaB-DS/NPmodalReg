47fa22b54f8b9fe842f966b0d640eed1a9575a62
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2022-02-13 12:04:41 +0530

    upadted meanshift algorithm

--- NPmodalReg/R/meanshift1d.R
+++ NPmodalReg/R/meanshift1d.R
@@ -1,22 +1,24 @@
-modalReg <- function(Xdata, Ydata, xgrid, ygrid, hx, hy, tol = 1e-6, iter = 100)
+modalReg <- function(Xdata, Ydata, xgrid = Xdata, ygrid = Ydata, hx = 0.01, hy = 0.01, tol = 1e-8, iter = 1000)
 {
   # initialization
   x0 = xgrid
   y0 = ygrid
   
   i = 0
-  diff = 0.5
+  diff = 1
   
   while(i < iter & diff > tol)
   {
     ytemp = y0
+    
     y0 = sapply(1:length(y0), 
-                function(x) sum(Ydata*dnorm(y0[x] - Ydata, sd = hy) * 
+                function(x) sum(Ydata * dnorm(y0[x] - Ydata, sd = hy) * 
                                   dnorm(x0[x] - Xdata, sd = hx)) / 
                   sum(dnorm(y0[x] - Ydata, sd = hy) * dnorm(x0[x] - Xdata, sd = hx)))
+    
+    i = i + 1
+    diff = max(abs(ytemp - y0))
   }
-  i = i + 1
-  diff = max(abs(ytemp - y0))
   
   return(y0)
 }
